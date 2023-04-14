54c199a9014afa094431acbb7039e675242594d5
fangzhen.zhou <fangzhen.zhou@ushow.media>
2023-04-14 09:15:50 +0800

    add union code

---
+++ alg/graph/union.go
@@ -0,0 +1,44 @@
+package graph
+
+type UF struct {
+	// 指向的父节点，一开始默认指向自己
+	parent []int
+	// 联通分量的个数
+	count int
+}
+
+func NewUF(n int) *UF {
+	parent := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+	}
+	return &UF{parent, n}
+}
+
+func (u *UF) Union(p, q int) {
+	rootP := u.parent[p]
+	rootQ := u.parent[q]
+	if rootP == rootQ {
+		return
+	}
+
+	u.parent[rootQ] = rootP
+	u.count--
+}
+
+func (u *UF) Find(x int) int {
+	// 路径压缩， 最终都指向跟节点
+	if x != u.parent[x] {
+		u.parent[x] = u.Find(u.parent[x])
+	}
+
+	return u.parent[x]
+}
+
+func (u *UF) Connected(p, q int) bool {
+	return u.Find(p) == u.Find(q)
+}
+
+func (u *UF) Count() int {
+	return u.count
+}
